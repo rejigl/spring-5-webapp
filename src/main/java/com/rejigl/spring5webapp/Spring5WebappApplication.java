5acb39b184b41e9d0c11acc24273b894d7e7c26a
Reji <rejigl@gmail.com>
2020-04-04 02:54:28 -0400

    Adding source code

---
+++ src/main/java/com/rejigl/spring5webapp/Spring5WebappApplication.java
@@ -0,0 +1,13 @@
+package com.rejigl.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+
+}
